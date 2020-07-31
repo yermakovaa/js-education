5e1cfe40457e765027d09bc7a9dbdb8cf65fc590
Anastasiia Yermakova <yermak1037@gmail.com>
2020-07-31 22:21:02 +0300

    Create codewars-task-04.js

---
+++ js/codewars-task-04.js
@@ -0,0 +1,33 @@
+/*
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
+*/
+
+function alphabetPosition(text) {
+  let array = [];
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  text = text.toLowerCase().split('');
+
+  for (let i = 0; i < text.length; i += 1) {
+    if (alphabet.includes(text[i])) {
+      array.push(alphabet.indexOf(text[i]) + 1);
+    }
+  }
+  return array.join(' ');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+// '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11';
+console.log(alphabetPosition('The narwhal bacons at midnight.'));
+// '20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20';
