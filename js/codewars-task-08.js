5bf326780f150b253a3bb4c5ca0519064ef651d7
Anastasiia Yermakova <yermak1037@gmail.com>
2020-08-02 12:47:33 +0300

    Create codewars-task-08.js

---
+++ js/codewars-task-08.js
@@ -0,0 +1,17 @@
+/* 
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+
+function opposite(number) {
+  const inverse = number * -1;
+  return inverse;
+}
+
+console.log(opposite(1));
+// -1
