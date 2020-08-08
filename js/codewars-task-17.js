dcb6e9d5ccbfec215984ba7438eeb594750b634b
Anastasiia Yermakova <yermak1037@gmail.com>
2020-08-09 01:42:05 +0300

    Create codewars-task-17.js

---
+++ js/codewars-task-17.js
@@ -0,0 +1,16 @@
+/*
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+numberToString(123); // returns '123';`   
+numberToString(999); // returns '999';`
+*/
+
+function numberToString(num) {
+  return String(num);
+}
+
+console.log(numberToString(67));
+// '67'
