381ce8717a77503daa3d9c4ca37c4c74df863bf1
Anastasiia Yermakova <yermak1037@gmail.com>
2020-08-09 01:42:08 +0300

    Create codewars-task-18.js

---
+++ js/codewars-task-18.js
@@ -0,0 +1,14 @@
+/*
+Description
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+*/
+
+const stringToNumber = function (str) {
+  // put your code here
+  return Number(str);
+};
+
+console.log(stringToNumber('1234'));
+// 1234
