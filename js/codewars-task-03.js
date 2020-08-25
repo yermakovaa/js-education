b57d62ba0b5eaca55d8f4ceb7915b13cb656b265
Anastasiia Yermakova <yermak1037@gmail.com>
2020-08-25 15:13:25 +0300

    Update codewars-task-03.js

--- js/codewars-task-03.js
+++ js/codewars-task-03.js
@@ -1,20 +1,9 @@
 /* Simple, given a string of words, return the length of the shortest word(s).
-
 String will never be empty and you do not need to account for different data types. 
 */
 
-function findShort(string) {
-  const arrarrayString = string.split(' ');
-  let smallestWord = arrarrayString[0];
-
-  for (let i = 0; i < arrarrayString.length; i += 1) {
-    if (arrarrayString[i].length < smallestWord.length) {
-      smallestWord = arrarrayString[i];
-    }
-  }
-
-  return smallestWord.length;
-}
+const findShort = string =>
+  string.split(' ').sort((a, b) => a.length - b.length)[0].length;
 
 console.log(findShort('bitcoin take over the world maybe who knows perhaps'));
 console.log(
@@ -22,3 +11,16 @@ console.log(
     'turns out random test cases are easier than writing out basic ones',
   ),
 );
+
+// function findShort(string) {
+//   const arrarrayString = string.split(' ');
+//   let smallestWord = arrarrayString[0];
+
+//   for (let i = 0; i < arrarrayString.length; i += 1) {
+//     if (arrarrayString[i].length < smallestWord.length) {
+//       smallestWord = arrarrayString[i];
+//     }
+//   }
+
+//   return smallestWord.length;
+// }
